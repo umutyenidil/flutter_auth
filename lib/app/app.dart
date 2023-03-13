42c9371c367042f21a1628a688eb5499f8acdd76
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-13 21:34:03 +0300

    app.dart has been updated
    some routes have been added

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_auth/constants/route_constants.dart';
 import 'package:flutter_auth/view/home_page/home_page.dart';
+import 'package:flutter_auth/view/sign_up_page/sign_up_page.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
@@ -16,8 +17,10 @@ class App extends StatelessWidget {
 
   Route? onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
-      case RouteConstants.HOME_PAGE:
-        return MaterialPageRoute(builder: (BuildContext context) => const HomePage());
+      case RouteConstants.homePageRoute:
+        return MaterialPageRoute(builder: (BuildContext context) => const SignUpPage());
+      case RouteConstants.signUpPageRoute:
+        return MaterialPageRoute(builder: (BuildContext context) => const SignUpPage());
       default:
         return MaterialPageRoute(builder: (BuildContext context) => const HomePage());
     }
