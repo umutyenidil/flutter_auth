56aa82bdeb1ea61a498754fef53540e6bee7cc0d
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-13 14:35:04 +0300

    the material app has been updated and home_page has been created

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_auth/view/home_page/home_page.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Auth',
+      onGenerateRoute: onGenerateRoute,
+    );
+  }
+
+  Route? onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (BuildContext context) => const HomePage());
+      default:
+        return MaterialPageRoute(builder: (BuildContext context) => const HomePage());
+    }
   }
 }
