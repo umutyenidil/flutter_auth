b949e7ee11f87ca3cc1ab672a2de807c978047fe
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-13 21:30:19 +0300

    build_context_extensions.dart has been updated
    SelectWidgetByPlatform extension has been created

--- lib/extensions/build_context_extensions.dart
+++ lib/extensions/build_context_extensions.dart
@@ -1,3 +1,5 @@
+import 'dart:io' show Platform;
+
 import 'package:flutter/material.dart';
 
 extension ScreenInformations on BuildContext {
@@ -7,5 +9,20 @@ extension ScreenInformations on BuildContext {
 
   double get screenWidth {
     return MediaQuery.of(this).size.width;
+  }
+}
+
+extension SelectWidgetByPlatform on BuildContext {
+  Widget selectWidgetByPlatform({
+    required Widget androidWidget,
+    required Widget iosWidget,
+  }) {
+    if (Platform.isAndroid) {
+      return androidWidget;
+    } else if (Platform.isIOS) {
+      return iosWidget;
+    } else {
+      throw Exception("The current platform isn't android or ios");
+    }
   }
 }
