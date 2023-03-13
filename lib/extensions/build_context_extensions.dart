1d9feaf63ffdc2c866b6be6b102e10c98ea14e97
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-13 15:04:02 +0300

    ScreenInformations extension has been created
    screenHeight getter and screenWidth getter has been created

---
+++ lib/extensions/build_context_extensions.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+extension ScreenInformations on BuildContext {
+  double get screenHeight {
+    return MediaQuery.of(this).size.height;
+  }
+
+  double get screenWidth {
+    return MediaQuery.of(this).size.width;
+  }
+}
