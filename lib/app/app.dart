f82ca8fe53d4493635ffff4263d55996bd2d0035
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-14 12:28:20 +0300

    app.dart has been updated
    some import paths have been changed

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_auth/constants/route_constants.dart';
-import 'package:flutter_auth/view/home_page/home_page.dart';
-import 'package:flutter_auth/view/sign_up_page/sign_up_page.dart';
+import 'package:flutter_auth/pages/home_page/home_page.dart';
+import 'package:flutter_auth/pages/sign_up_page/sign_up_page.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
@@ -18,9 +18,9 @@ class App extends StatelessWidget {
   Route? onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case RouteConstants.homePageRoute:
-        return MaterialPageRoute(builder: (BuildContext context) => const SignUpPage());
+        return MaterialPageRoute(builder: (BuildContext context) => SignUpPage());
       case RouteConstants.signUpPageRoute:
-        return MaterialPageRoute(builder: (BuildContext context) => const SignUpPage());
+        return MaterialPageRoute(builder: (BuildContext context) => SignUpPage());
       default:
         return MaterialPageRoute(builder: (BuildContext context) => const HomePage());
     }
