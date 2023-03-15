346d9e0f873fc88797afe634deabff86f95df5b8
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-15 23:09:51 +0300

    route_constants.dart has been updated
    errorPageRoute constant has been added

--- lib/constants/route_constants.dart
+++ lib/constants/route_constants.dart
@@ -1,4 +1,5 @@
 class RouteConstants {
   static const String homePageRoute = '/';
   static const String signUpPageRoute = '/sign-up-page/';
+  static const String errorPageRoute = '/error-page/';
 }
