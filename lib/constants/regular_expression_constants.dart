5c868f18e8584c96b3e83f17b39953e4ff57ee62
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-15 23:40:13 +0300

    regular_expression_constants.dart has been updated
    min8CharacterWithAnythingRegex constant has been added

--- lib/constants/regular_expression_constants.dart
+++ lib/constants/regular_expression_constants.dart
@@ -1,4 +1,5 @@
 class RegularExpressionConstants {
+  static const String min8CharacterWithAnythingRegex = r'^.{8,}$';
   static const String emailRegex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$';
   static const String min8CharacterRegex = r'^.{8,}$';
   static const String min8CharacterWithJustLettersAndNumbers = r'^[a-zA-Z0-9]{8,}$';
