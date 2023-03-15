ed36b8145bd3d053f59a0ed8af8867c29deb85c8
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-15 23:39:34 +0300

    error_message_constants.dart has been updated
    passwordInputFieldErrorMessage2 constant has been added

--- lib/constants/error_message_constants.dart
+++ lib/constants/error_message_constants.dart
@@ -2,5 +2,6 @@ class ErrorMessageConstants {
   static const String usernameInputFieldErrorMessage = 'Kullanici adi minimum 8 karakterden olusup sadece rakam ve harf icermelidir.';
   static const String emailInputFieldErrorMessage = 'Lutfen gecerli bir mail adresi giriniz.';
   static const String passwordInputFieldErrorMessage = 'Parolaniz minimum 6 karakterli olmalidir';
+  static const String passwordInputFieldErrorMessage2 = 'Gecerli bir parola giriniz.';
   static const String passwordAgainInputFieldErrorMessage = 'Parolalar eslesmiyor';
 }
