9eff968337a4f27366598d37750cdf68e8127661
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-20 22:53:03 +0300

    app.dart has been updated
    Auth bloc has been injected to MaterialApp with MultiBlocProvider

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_auth/blocs/auth_bloc.dart';
 import 'package:flutter_auth/constants/route_constants.dart';
 import 'package:flutter_auth/pages/error_page/error_page.dart';
 import 'package:flutter_auth/pages/sign_in_page/sign_in_page.dart';
 import 'package:flutter_auth/pages/sign_up_page/sign_up_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Auth',
-      onGenerateRoute: onGenerateRoute,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthBloc>(
+          create: (BuildContext context) => AuthBloc(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Auth',
+        onGenerateRoute: onGenerateRoute,
+      ),
     );
   }
 
