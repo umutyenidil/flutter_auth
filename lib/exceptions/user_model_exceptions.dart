f88efa7668a4061849b8bb2bd9e5803aa1915934
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-20 22:56:41 +0300

    user_model_exceptions.dart has been updated
    UserEmailAlreadyInUseException,
    UserInvalidEmailException,
    UserOperationNotAllowedException,
    UserWeakPasswordException
    have been created

--- lib/exceptions/user_model_exceptions.dart
+++ lib/exceptions/user_model_exceptions.dart
@@ -3,3 +3,11 @@ class UserHasNotBeenCreatedException implements Exception {}
 class UserDocumentNotFoundException implements Exception {}
 
 class UserDetailDocumentNotFoundException implements Exception {}
+
+class UserEmailAlreadyInUseException implements Exception {}
+
+class UserInvalidEmailException implements Exception {}
+
+class UserOperationNotAllowedException implements Exception {}
+
+class UserWeakPasswordException implements Exception {}
