0461ee8d20b7b3eeeb0cb66b1e151302ab4a2290
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-22 01:57:55 +0300

    error_message_contants.dart has been updated

--- lib/constants/error_message_constants.dart
+++ lib/constants/error_message_constants.dart
@@ -1,7 +1,7 @@
 class ErrorMessageConstants {
-  static const String usernameInputFieldErrorMessage = 'Kullanici adi minimum 8 karakterden olusup sadece rakam ve harf icermelidir.';
-  static const String emailInputFieldErrorMessage = 'Lutfen gecerli bir mail adresi giriniz.';
-  static const String passwordInputFieldErrorMessage = 'Parolaniz minimum 6 karakterli olmalidir';
-  static const String passwordInputFieldErrorMessage2 = 'Gecerli bir parola giriniz.';
-  static const String passwordAgainInputFieldErrorMessage = 'Parolalar eslesmiyor';
+  static const String usernameInputFieldErrorMessage = 'Kullanıcı adı minimum 8 karakterden oluşup sadece rakam ve harf içermelidir.';
+  static const String emailInputFieldErrorMessage = 'Lütfen geçerli bir email adresi giriniz.';
+  static const String passwordInputFieldErrorMessage = 'Parolanız minimum 6 karakterli olmalıdır';
+  static const String passwordInputFieldErrorMessage2 = 'Geçerli bir parola giriniz.';
+  static const String passwordAgainInputFieldErrorMessage = 'Girilen parolalar eşleşmiyor!';
 }
