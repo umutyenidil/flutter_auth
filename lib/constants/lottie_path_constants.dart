34b6af5b871501d8f17b80e1897d3fc61ae2596a
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-22 01:47:55 +0300

    some lotties have been added
    progress_indicator_lottie.json,
    progress_indicator_gear_lottie.json
    have been added

--- lib/constants/lottie_path_constants.dart
+++ lib/constants/lottie_path_constants.dart
@@ -1,4 +1,5 @@
 class LottiePathConstants {
   static const String errorLottie = 'assets/lotties/error_lottie.json';
   static const String progressIndicatorLottie = 'assets/lotties/progress_indicator_lottie.json';
+  static const String progressIndicatorGearLottie = 'assets/lotties/progress_indicator_gear_lottie.json';
 }
