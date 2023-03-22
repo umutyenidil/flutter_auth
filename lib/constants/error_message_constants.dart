5cc373428be38800bfabbdfe124c6da9af26006a
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-22 13:39:40 +0300

    error_message_constants.dart has been updated
    minimum character limit has been updated to 8

--- lib/constants/error_message_constants.dart
+++ lib/constants/error_message_constants.dart
@@ -1,7 +1,7 @@
 class ErrorMessageConstants {
   static const String usernameInputFieldErrorMessage = 'Kullanıcı adı minimum 8 karakterden oluşup sadece rakam ve harf içermelidir.';
   static const String emailInputFieldErrorMessage = 'Lütfen geçerli bir email adresi giriniz.';
-  static const String passwordInputFieldErrorMessage = 'Parolanız minimum 6 karakterli olmalıdır';
+  static const String passwordInputFieldErrorMessage = 'Parolanız minimum 8 karakterli olmalıdır';
   static const String passwordInputFieldErrorMessage2 = 'Geçerli bir parola giriniz.';
   static const String passwordAgainInputFieldErrorMessage = 'Girilen parolalar eşleşmiyor!';
 }
