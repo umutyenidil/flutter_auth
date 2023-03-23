925848fb4284b8870bf5a01076dbc8694d3d24e4
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-23 16:19:23 +0300

    new routes have been added
    emailVerificationPageRoute,
    profilePageRoute
    have been added

--- lib/constants/route_constants.dart
+++ lib/constants/route_constants.dart
@@ -3,4 +3,6 @@ class RouteConstants {
   static const String signUpPageRoute = '/sign-up-page/';
   static const String signInPageRoute = '/sign-in-page/';
   static const String errorPageRoute = '/error-page/';
+  static const String emailVerificationPageRoute = '/email-verification-page/';
+  static const String profilePageRoute = '/profile-page/';
 }
