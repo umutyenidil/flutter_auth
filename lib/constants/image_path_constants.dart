8f7a04b3f7db9df39cc580fd860d4026ad748fb8
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-23 16:13:46 +0300

    some images have been added

--- lib/constants/image_path_constants.dart
+++ lib/constants/image_path_constants.dart
@@ -1,3 +1,4 @@
 class ImagePathConstants {
   static const String authBackgroundImage = 'assets/images/auth_background_image.svg';
+  static const String verifyEmailImage = 'assets/images/verify_email_image.svg';
 }
