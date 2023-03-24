88fd803fc5e8638f5c2392d0e5f8d8f3837727b7
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-24 14:24:29 +0300

    update: app.dart
    new routes has been added

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_auth/blocs/auth_bloc.dart';
 import 'package:flutter_auth/constants/route_constants.dart';
+import 'package:flutter_auth/pages/email_verification_page/email_verification_page.dart';
 import 'package:flutter_auth/pages/error_page/error_page.dart';
+import 'package:flutter_auth/pages/profile_page/profile_page.dart';
 import 'package:flutter_auth/pages/sign_in_page/sign_in_page.dart';
 import 'package:flutter_auth/pages/sign_up_page/sign_up_page.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -21,6 +23,9 @@ class App extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Flutter Auth',
         onGenerateRoute: onGenerateRoute,
+        theme: ThemeData(
+          fontFamily: 'Urbanist',
+        ),
       ),
     );
   }
@@ -28,13 +33,17 @@ class App extends StatelessWidget {
   Route? onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case RouteConstants.homePageRoute:
-        return MaterialPageRoute(builder: (BuildContext context) => const SignUpPage());
+        return MaterialPageRoute(builder: (BuildContext context) => const SignInPage());
       case RouteConstants.signUpPageRoute:
         return MaterialPageRoute(builder: (BuildContext context) => const SignUpPage());
       case RouteConstants.signInPageRoute:
         return MaterialPageRoute(builder: (BuildContext context) => const SignInPage());
       case RouteConstants.errorPageRoute:
         return MaterialPageRoute(builder: (BuildContext context) => const ErrorPage());
+      case RouteConstants.emailVerificationPageRoute:
+        return MaterialPageRoute(builder: (BuildContext context) => const EmailVerificationPage());
+      case RouteConstants.profilePageRoute:
+        return MaterialPageRoute(builder: (BuildContext context) => const ProfilePage());
       default:
         return MaterialPageRoute(builder: (BuildContext context) => const ErrorPage());
     }
