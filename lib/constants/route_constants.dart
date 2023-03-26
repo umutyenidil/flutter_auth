8123d4b770c896d394c209cac1ac59109ae4cece
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-26 16:50:48 +0300

    update: route_constants.dart
    some routes have been changed,
    new routes have been added

--- lib/constants/route_constants.dart
+++ lib/constants/route_constants.dart
@@ -1,8 +1,10 @@
 class RouteConstants {
-  static const String homePageRoute = '/';
+  static const String homePageRoute = '/home-page/';
   static const String signUpPageRoute = '/sign-up-page/';
   static const String signInPageRoute = '/sign-in-page/';
   static const String errorPageRoute = '/error-page/';
   static const String emailVerificationPageRoute = '/email-verification-page/';
   static const String profilePageRoute = '/profile-page/';
+  static const String createProfilePageRoute = '/create-profile-page/';
+  static const String introductionPageRoute = '/';
 }
