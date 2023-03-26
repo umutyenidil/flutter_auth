5918a996f01bd24c63989851b2119d5c90c67fd6
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-26 17:02:08 +0300

    update: home_page.dart

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_auth/constants/icon_path_constants.dart';
+import 'package:flutter_auth/constants/route_constants.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: const Center(
+        child: Text(
+          'home page',
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: SizedBox.square(
+          dimension: 35,
+          child: SvgPicture.asset(
+            IconPathConstants.userIcon,
+            color: Colors.white,
+          ),
+        ),
+        onPressed: () {
+          Navigator.of(context).pushNamedAndRemoveUntil(
+            RouteConstants.profilePageRoute,
+            (route) => false,
+          );
+        },
+      ),
+    );
   }
 }
