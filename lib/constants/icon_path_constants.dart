a1e7836851145ee6204e19aaed71297bba00a94b
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-26 16:42:53 +0300

    asset: new icons

--- lib/constants/icon_path_constants.dart
+++ lib/constants/icon_path_constants.dart
@@ -1,5 +1,11 @@
 class IconPathConstants {
   static const String chevronLeftIcon = 'assets/icons/chevron_left_icon.svg';
+  static const String chevronRightIcon = 'assets/icons/chevron_right_icon.svg';
+  static const String chevronTopIcon = 'assets/icons/chevron_top_icon.svg';
+  static const String chevronBottomIcon = 'assets/icons/chevron_bottom_icon.svg';
+  static const String editIcon = 'assets/icons/edit_icon.svg';
+  static const String galleryIcon = 'assets/icons/gallery_icon.svg';
+  static const String cameraIcon = 'assets/icons/camera_icon.svg';
   static const String appleLogoIcon = 'assets/icons/apple_logo_icon.svg';
   static const String googleLogoIcon = 'assets/icons/google_logo_icon.svg';
   static const String facebookLogoIcon = 'assets/icons/facebook_logo_icon.svg';
@@ -13,4 +19,5 @@ class IconPathConstants {
   static const String checkIcon = 'assets/icons/check_icon.svg';
   static const String infoIcon = 'assets/icons/info_icon.svg';
   static const String logoutIcon = 'assets/icons/logout_icon.svg';
+  static const String circleUserIcon = 'assets/icons/circle_user_icon.svg';
 }
