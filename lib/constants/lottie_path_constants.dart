5b634d66339e6cda15bf81d2cf608771c0a0f2cd
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-27 15:59:49 +0300

    asset: loadingLottie and clockLottie has been added

--- lib/constants/lottie_path_constants.dart
+++ lib/constants/lottie_path_constants.dart
@@ -3,4 +3,6 @@ class LottiePathConstants {
   static const String progressIndicatorLottie = 'assets/lotties/progress_indicator_lottie.json';
   static const String progressIndicatorGearLottie = 'assets/lotties/progress_indicator_gear_lottie.json';
   static const String introLottie = 'assets/lotties/intro_lottie.json';
+  static const String loadingLottie = 'assets/lotties/loading_lottie.json';
+  static const String clockLottie = 'assets/lotties/clock_lottie.json';
 }
