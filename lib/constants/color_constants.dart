44502f83955110dbd95fc4274df367d2a44f49a2
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-30 16:05:20 +0300

    update: color constants
    random color has been added

--- lib/constants/color_constants.dart
+++ lib/constants/color_constants.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
 
 class ColorConstants {
   static Color popUpBarrierColor = Colors.grey.withOpacity(0.5);
+  static Color randomColor = Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
 }
