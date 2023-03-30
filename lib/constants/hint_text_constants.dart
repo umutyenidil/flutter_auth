cb7d7530a76289883ba2a5ac30f0217037e1212f
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-30 16:02:46 +0300

    create: hint text constants

---
+++ lib/constants/hint_text_constants.dart
@@ -0,0 +1,5 @@
+class HintTextConstants {
+  static const String emailAddressInputFieldHintText = 'Enter your email address';
+  static const String passwordInputFieldHintText = 'Enter your password';
+  static const String passwordAgainInputFieldHintText = 'Enter your password again';
+}
