f222956e8a0e30025260d8f83d64ea386a051bfd
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-30 16:08:27 +0300

    update: image storage mixin

--- lib/mixins/image_storage_mixin.dart
+++ lib/mixins/image_storage_mixin.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -20,5 +22,36 @@ mixin ImageStorage {
     ByteData bytes = await rootBundle.load(assetPath);
     Uint8List imageBytes = bytes.buffer.asUint8List();
     return imageBytes;
+  }
+
+  Future<File?> getPhotoFileFromCamera() async {
+    ImagePicker picker = ImagePicker();
+    XFile? photo = await picker.pickImage(
+      source: ImageSource.camera,
+      imageQuality: 50,
+      preferredCameraDevice: CameraDevice.front,
+    );
+
+    if (photo == null) {
+      return null;
+    }
+
+    File photoFile = File(photo.path);
+    return photoFile;
+  }
+
+  Future<File?> getPhotoFileFromGallery() async {
+    ImagePicker picker = ImagePicker();
+    XFile? photo = await picker.pickImage(
+      source: ImageSource.gallery,
+      imageQuality: 50,
+    );
+
+    if (photo == null) {
+      return null;
+    }
+
+    File photoFile = File(photo.path);
+    return photoFile;
   }
 }
