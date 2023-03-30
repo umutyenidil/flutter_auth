09ff802a62a32671302affa26bd21c598b21a703
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-30 16:03:10 +0300

    update: regular expression operations

--- lib/constants/regular_expression_constants.dart
+++ lib/constants/regular_expression_constants.dart
@@ -1,7 +1,6 @@
 class RegularExpressionConstants {
-  static const String min8CharacterWithAnythingRegex = r'^.{8,}$';
-  static const String emailRegex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$';
-  static const String min8CharacterRegex = r'^.{8,}$';
+  static const String min8Char = r'^.{8,}$';
+  static const String emailAddress = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$';
   static const String min8CharacterWithJustLettersAndNumbers = r'^[a-zA-Z0-9]{8,}$';
 
   static String toRegex(String text) {
---
+++ lib/constants/regular_expression_warning_text_constants.dart
@@ -0,0 +1,6 @@
+class RegularExpressionWarningTextConstants {
+  static String min8CharWarningText({required String fieldName}) {
+    return '${fieldName} must contain at least 8 characters';
+  }
+
+}
