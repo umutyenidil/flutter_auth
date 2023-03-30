a06f19bb972c5820deb66c673d9c3230a583f643
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-30 16:05:57 +0300

    update: pop up extensions
    pop up barrier color has been changed

--- lib/extensions/pop_up_extensions.dart
+++ lib/extensions/pop_up_extensions.dart
@@ -2,12 +2,14 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_auth/common_widgets/pop_ups/pop_up.dart';
+import 'package:flutter_auth/constants/color_constants.dart';
 
 extension Show on PopUp {
   Future<dynamic> show(BuildContext context) async {
     return await showDialog(
       context: context,
       barrierDismissible: false,
+      barrierColor: ColorConstants.popUpBarrierColor,
       builder: (BuildContext context) {
         return BackdropFilter(
           filter: ImageFilter.blur(
