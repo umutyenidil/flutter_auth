7a0f2fe6656ceec5de69133bb2355c49d35302ed
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-31 07:40:50 +0300

    update: auth bloc

--- lib/blocs/auth_bloc/auth_bloc.dart
+++ lib/blocs/auth_bloc/auth_bloc.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:developer' as devtools show log;
 
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_auth/exceptions/user_model_exceptions.dart';
