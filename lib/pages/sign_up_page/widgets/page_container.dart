e3c5f65753f21fe2abcac02d477e5a433e08d632
Umut Yenidil <umut.yenidil9@gmail.com>
2023-03-31 07:36:45 +0300

    update: sign up page
    some importations have been removed

--- lib/pages/sign_up_page/widgets/page_container.dart
+++ lib/pages/sign_up_page/widgets/page_container.dart
@@ -1,10 +1,7 @@
-import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_auth/constants/image_path_constants.dart';
 import 'package:flutter_auth/extensions/build_context_extensions.dart';
 import 'package:flutter_auth/extensions/single_child_scroll_view_extensions.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class PageContainer extends StatelessWidget {
   const PageContainer({Key? key, required this.content}) : super(key: key);
